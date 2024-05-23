6cdabbd576314110ffcefa8a37705259c3aa6961
wodemaja <cosina.hallo@gmail.com>
2024-05-23 20:01:43 +0200

    Update Likelihood analysis

--- LikelihoodAnalysis.py
+++ LikelihoodAnalysis.py
@@ -37,6 +37,7 @@ def preprocessing_data(dataframe,Emin):
 
     print(f'Dropping low-energy values below {Emin}')
     dataframe = dataframe[dataframe['energy']>Emin]
+    dataframe = dataframe[dataframe['E.trks.lik[:,0]']>0]
 
     #Renaming columns
     dataframe.rename(columns = {"E.trks.lik[:,0]" : "Likelihood track" , "E.trks.lik[:,1]" : "Likelihood shower"}, inplace=True)
@@ -86,33 +87,46 @@ def plotting_hist_scatter(dataframe):
     xline = np.linspace(0.001,xmax,1000)
     yline = np.linspace(0.001,-ymax,1000)
 
-    h1 = ax5.hist2d(x,y, bins=300,cmap='viridis')
-    ax5.set_xlabel('Likelihood track')
-    ax5.set_ylabel('Likelihood shower')
+    h1 = ax5.hist2d(x,y, bins=300,cmap='viridis') #range=[[0,200],[-200,0]]
+    ax5.set_xlabel('-log Likelihood track')
+    ax5.set_ylabel('log Likelihood shower')
     ax5.set_title('2d histogram')
     fig2.colorbar(h1[3])
     #ax5.plot(-np.log(xline),np.log(yline),color='red')
 
-    h2 = ax6.hist2d(x,y, bins=300, range=[[0,200],[-200,0]],cmap='viridis')
-    ax6.set_xlabel('Likelihood track')
-    ax6.set_ylabel('Likelihood shower')
+    h2 = ax6.hist2d(x,y, bins=300,cmap='viridis') #range=[[0,200],[-200,0]]
+    ax6.set_xlabel('-log Likelihood track')
+    ax6.set_ylabel('log Likelihood shower')
     ax6.set_title('2d histogram')
     fig2.colorbar(h2[3])
-    #ax6.plot(xline,yline,color='red')
+    #ax5.plot(-np.log(xline),np.log(yline),color='red')
 
-    h3 = ax7.hist2d(x,y, bins=100, range=[[0,100],[-100,0]],cmap='viridis')
+    h3 = ax7.hist2d(x_exp,y_exp, bins=26, range=[[0,0.4],[0,0.4]],norm='asinh',cmap='viridis')
     ax7.set_xlabel('Likelihood track')
     ax7.set_ylabel('Likelihood shower')
-    ax7.set_title('2d histogram')
+    ax7.set_title('2d histogram, 26 bins')
     fig2.colorbar(h3[3])
-    #ax7.plot(xline,yline,color='red')
 
-    h4 = ax8.hist2d(x_exp,y_exp,range = [[0,0.02*np.power(10,14)],[0,0.01]], bins=100,norm='linear',cmap='viridis') #range=[[20,75],[-125,-25]]
+    h4 = ax8.hist2d(x_exp,y_exp, bins=13,range=[[0,0.2],[0,0.2]],norm='asinh',cmap='viridis')
     ax8.set_xlabel('Likelihood track')
     ax8.set_ylabel('Likelihood shower')
-    ax8.set_title('2d histogram')
+    ax8.set_title('2d histogram, 13 bins')
     fig2.colorbar(h4[3])
-    #ax8.plot(xline,yline,color='red')
+
+    # x_density_array = h3[0][:]
+    # y_density_array = h3[:][0]
+
+    # h2_1 = ax6_1.scatter(np.arange(stop=30),x_density_array)
+    # ax6_1.set_xlabel('bin number')
+    # ax6_1.set_ylabel('Amount of binned values')
+    # ax6_1.set_title('Scatter plot')
+
+    # print(len(np.arange(0, len(y_density_array)-1)))
+    # print(np.shape(y_density_array))
+    # h2_2 = ax6_2.scatter(np.arange(0, len(y_density_array)-1),y_density_array)
+    # ax6_2.set_xlabel('bin number')
+    # ax6_2.set_ylabel('Amount of binned values')
+    # ax6_2.set_title('Scatter plot')
 
     fig2.suptitle(f'Likelihood histograms with energy > {Emin}')
 
@@ -121,7 +135,7 @@ def plotting_hist_scatter(dataframe):
 
 
 
-Emin = 9000
+Emin = 6000
 dataframe = preprocessing_data(dataframe, Emin)
 
 plotting_hist_scatter(dataframe)
