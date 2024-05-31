ba9790d65a7a3f086f5c96779c6aa9b566d9cbc5
wodemaja <cosina.hallo@gmail.com>
2024-05-31 23:46:13 +0200

    Cleaned up code and functions with docstrings. Option to plot plots separately added.

--- LikelihoodAnalysis.py
+++ LikelihoodAnalysis.py
@@ -19,13 +19,15 @@ def preprocessing_data(dataframe,Emin):
 
     Arguments
     --------
-    input : DataFrame
+    input   : pd.DataFrame
         Name of dataframe
+    Emin    : int
+        Lower energy threshold for data to be taken into account.
 
     Returns
     -------
-    newname : int
-        Lower threshold for energy values to taken into account.
+    dataframe : pd.DataFrame
+        Pre-processed dataframe only containing higher energy values.
     '''
     #remove rows with missing values
     print("Removing missing values ({} now)".format(dataframe.isnull().values.sum()))
@@ -33,117 +35,155 @@ def preprocessing_data(dataframe,Emin):
     print("Missing values: {}".format(dataframe.isnull().values.sum()))
 
     print('Only retaining relevant columns related to likelihood and energy')
-    dataframe = dataframe.filter(["E.trks.lik[:,0]", "E.trks.lik[:,1]","energy"])
+    dataframe = dataframe.filter(["Track reconstruction likelyhood", "Shower reconstruction likelyhood","energy","Is shower?"])
 
     print(f'Dropping low-energy values below {Emin}')
     dataframe = dataframe[dataframe['energy']>Emin]
-    dataframe = dataframe[dataframe['E.trks.lik[:,0]']>0]
-
-    #Renaming columns
-    dataframe.rename(columns = {"E.trks.lik[:,0]" : "Likelihood track" , "E.trks.lik[:,1]" : "Likelihood shower"}, inplace=True)
+    dataframe = dataframe[dataframe["Track reconstruction likelyhood"]>0]
 
     return dataframe
 
-def plotting_hist_scatter(dataframe):
+
+def plotting_hist_scatter(dataframe,separated):
     '''
-    Plotting a scatter plot of the likelihood data track vs. shower and 2d histograms in chosen likelihood ranges.
-    Plotting histograms of track and shower likelihood.
+    Plotting a scatter plot of the likelihood data track vs. shower.
+    Plotting 2d histograms of track and shower likelihood.
+
+    Arguments
+    --------
+    dataframe   : DataFrame
+        Name of dataframe
+    separated   : bool
+        Set type of plot. If 'True', then plots separated plots, if 'False' plots come in two figures.
     '''
-    x = dataframe["Likelihood track"]
-    y = dataframe["Likelihood shower"]
+    x = dataframe["Track reconstruction likelyhood"]
+    y = dataframe["Shower reconstruction likelyhood"]
 
     x_exp = np.exp(-x)
     y_exp = np.exp(y)
 
-    xmax = max(np.exp(-x))
-    ymax = max(np.exp(y))
+    df_shower   = dataframe[dataframe["Is shower?"]==True]
+    x_shower    = df_shower["Track reconstruction likelyhood"]
+    y_shower    = df_shower["Shower reconstruction likelyhood"]
+    df_track    = dataframe[dataframe["Is shower?"]==False]
+    x_track     = df_track["Track reconstruction likelyhood"]
+    y_track     = df_track["Shower reconstruction likelyhood"]
 
-    fig1, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2,2,figsize=(15,15))
+    if separated==True :
+        plt.figure(num=1,figsize=(7, 4))
+        plt.scatter(x,y,marker='.',c='C2',s=2)
+        plt.xlabel('Track reconstruction likelihood')
+        plt.ylabel('Shower reconstruction likelihood')
+        plt.title(f'Scatter plot for track and shower events (unmarked) with energy > {Emin}')
+        plt.tight_layout()
 
-    ax1.scatter(x,y)
-    ax1.set_xlabel('Likelihood track')
-    ax1.set_ylabel('Likelihood shower')
-    ax1.set_title('Scatter plot')
+        plt.figure(num=2,figsize=(6, 4))
+        plt.scatter(x_track,y_track,marker='.',label='Track event',s=2)
+        plt.scatter(x_shower,y_shower,marker='.',label='Shower event',c='orange',s=2)
+        plt.xlabel('Track reconstruction likelihood')
+        plt.ylabel('Shower reconstruction likelihood')
+        plt.title(f'Scatter plot for track and shower events with energy > {Emin}')
+        plt.legend()
+        plt.tight_layout()
 
-    ax2.scatter(x_exp,y_exp)
-    ax2.set_xlabel('Likelihood track')
-    ax2.set_ylabel('Likelihood shower')
-    ax2.set_title('Scatter plot with exp(-x),exp(y)')
+        plt.figure(num=3,figsize=(6, 4))
+        plt.scatter(x_track,y_track,marker='.',s=2)
+        plt.xlabel('Track reconstruction likelihood')
+        plt.ylabel('Shower reconstruction likelihood')
+        plt.title(f'Scatter plot for track events with energy > {Emin}')
+        plt.tight_layout()
 
-    ax3.hist(x)
-    ax3.set_xlabel('Likelihood track')
-    ax3.set_title('Histogram : track likelihood')
+        plt.figure(num=4,figsize=(6, 4))
+        plt.scatter(x_shower,y_shower,marker='.',c='orange',s=2)
+        plt.xlabel('Track reconstruction likelihood')
+        plt.ylabel('Shower reconstruction likelihood')
+        plt.title(f'Scatter plot for shower events with energy > {Emin}')
+        plt.tight_layout()
 
-    ax4.hist(y)
-    ax4.set_xlabel('Likelihood shower')
-    ax4.set_title('Histogram : shower likelihood')
+        fig1 = plt.figure(num=5,figsize=(6, 4))
+        h1 = plt.hist2d(x_track,y_track, bins=100,range=[[0,3000],[-2500,0]],norm='asinh',cmap='viridis')
+        plt.xlabel('Track reconstruction likelihood')
+        plt.ylabel('Shower reconstruction likelihood')
+        plt.title(f'2d histogram, track data with energy > {Emin}')
+        fig1.colorbar(h1[3])
+        plt.tight_layout()
 
-    fig1.suptitle(f'Likelihood histograms with energy > {Emin}')
+        fig2 = plt.figure(num=6,figsize=(6, 4))
+        h2 = plt.hist2d(x_shower,y_shower, bins=100,range=[[0,3000],[-2500,0]],norm='asinh',cmap='viridis')
+        plt.xlabel('Track reconstruction likelihood')
+        plt.ylabel('Shower reconstruction likelihood')
+        plt.title(f'2d histogram, shower data with energy > {Emin}')
+        fig2.colorbar(h2[3])
+        plt.tight_layout()
 
-    plt.show()
+        plt.show()
+    
+    else:
+        fig1, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2,2,figsize=(15,15))
 
+        ax1.scatter(x,y,c='C2',s=2)
+        ax1.set_xlabel('Track reconstruction likelihood')
+        ax1.set_ylabel('Shower reconstruction likelihood')
+        ax1.set_title('Scatter plot for track and shower events (unmarked)')
 
-    fig2, [[ax5, ax6], [ax7, ax8]] = plt.subplots(2,2,figsize=(15,15))
-    xline = np.linspace(0.001,xmax,1000)
-    yline = np.linspace(0.001,-ymax,1000)
+        ax2.scatter(x_track,y_track,marker='.',label='Track event',s=2)
+        ax2.scatter(x_shower,y_shower,marker='.',label='Shower event',c='orange',s=2)
+        ax2.set_xlabel('Track reconstruction likelihood')
+        ax2.set_ylabel('Shower reconstruction likelihood')
+        ax2.set_title('Scatter plot for track and shower events')
+        ax2.legend()
 
-    h1 = ax5.hist2d(x,y, bins=300,cmap='viridis') #range=[[0,200],[-200,0]]
-    ax5.set_xlabel('-log Likelihood track')
-    ax5.set_ylabel('log Likelihood shower')
-    ax5.set_title('2d histogram')
-    fig2.colorbar(h1[3])
-    #ax5.plot(-np.log(xline),np.log(yline),color='red')
+        ax3.scatter(x_track,y_track,marker='.',label='Track event',s=2)
+        ax3.set_xlabel('Track reconstruction likelihood')
+        ax3.set_ylabel('Shower reconstruction likelihood')
+        ax3.set_title('Scatter plot for track events')
+        ax3.legend()
 
-    h2 = ax6.hist2d(x,y, bins=300,cmap='viridis') #range=[[0,200],[-200,0]]
-    ax6.set_xlabel('-log Likelihood track')
-    ax6.set_ylabel('log Likelihood shower')
-    ax6.set_title('2d histogram')
-    fig2.colorbar(h2[3])
-    #ax5.plot(-np.log(xline),np.log(yline),color='red')
+        ax4.scatter(x_shower,y_shower,marker='.',label='Shower event',c='orange',s=2)
+        ax4.set_xlabel('Track reconstruction likelihood')
+        ax4.set_ylabel('Shower reconstruction likelihood')
+        ax4.set_title('Scatter plot for shower events')
+        ax4.legend()
 
-    h3 = ax7.hist2d(x_exp,y_exp, bins=26, range=[[0,0.4],[0,0.4]],norm='asinh',cmap='viridis')
-    ax7.set_xlabel('Likelihood track')
-    ax7.set_ylabel('Likelihood shower')
-    ax7.set_title('2d histogram, 26 bins')
-    fig2.colorbar(h3[3])
+        fig1.suptitle(f'Likelihood scatter plots for data with energy > {Emin}')
 
-    h4 = ax8.hist2d(x_exp,y_exp, bins=13,range=[[0,0.2],[0,0.2]],norm='asinh',cmap='viridis')
-    ax8.set_xlabel('Likelihood track')
-    ax8.set_ylabel('Likelihood shower')
-    ax8.set_title('2d histogram, 13 bins')
-    fig2.colorbar(h4[3])
-
-    # x_density_array = h3[0][:]
-    # y_density_array = h3[:][0]
-
-    # h2_1 = ax6_1.scatter(np.arange(stop=30),x_density_array)
-    # ax6_1.set_xlabel('bin number')
-    # ax6_1.set_ylabel('Amount of binned values')
-    # ax6_1.set_title('Scatter plot')
-
-    # print(len(np.arange(0, len(y_density_array)-1)))
-    # print(np.shape(y_density_array))
-    # h2_2 = ax6_2.scatter(np.arange(0, len(y_density_array)-1),y_density_array)
-    # ax6_2.set_xlabel('bin number')
-    # ax6_2.set_ylabel('Amount of binned values')
-    # ax6_2.set_title('Scatter plot')
-
-    fig2.suptitle(f'Likelihood histograms with energy > {Emin}')
-
-
-    plt.show()
-
+        plt.show()
 
 
-Emin = 6000
+        fig2, [[ax5,ax6], [ax7, ax8]] = plt.subplots(2,2,figsize=(15,15))
+
+        h1 = ax5.hist2d(x,y, bins=100,range=[[0,3000],[-2500,0]],norm='asinh',cmap='viridis')
+        ax5.set_xlabel('Track reconstruction likelihood')
+        ax5.set_ylabel('Shower reconstruction likelihood')
+        ax5.set_title('2d histogram, all data')
+        fig2.colorbar(h1[3])
+
+        ax6.scatter(x,y,marker='.',c='orange',s=2)
+        ax6.set_xlabel('Track reconstruction likelihood')
+        ax6.set_ylabel('Shower reconstruction likelihood')
+        ax6.set_title('Scatter plot for track and shower events')
+        ax6.legend()
+
+        h3 = ax7.hist2d(x_track,y_track, bins=100,range=[[0,3000],[-2500,0]],norm='asinh',cmap='viridis')
+        ax7.set_xlabel('Track reconstruction likelihood')
+        ax7.set_ylabel('Shower reconstruction likelihood')
+        ax7.set_title('2d histogram, track data')
+        fig2.colorbar(h3[3])
+
+        h4 = ax8.hist2d(x_shower,y_shower, bins=100,range=[[0,3000],[-2500,0]],norm='asinh',cmap='viridis')
+        ax8.set_xlabel('Track reconstruction likelihood')
+        ax8.set_ylabel('Shower reconstruction likelihood')
+        ax8.set_title('2d histogram, shower data')
+        fig2.colorbar(h4[3])
+
+        fig2.suptitle(f'Likelihood scatter plots and histograms with energy > {Emin}')
+
+
+        plt.show()
+
+
+
+Emin = 9000
 dataframe = preprocessing_data(dataframe, Emin)
 
-plotting_hist_scatter(dataframe)
-
-
-# print("Printing columns")
-# for col in dataframe.columns:
-#     print(col)
-
-# print(f'The maximum absolute value of track likelihood is {xmax}')
-# print(f'The maximum absolute value of shower likelihood is {ymax}')
+plotting_hist_scatter(dataframe,separated=True)
